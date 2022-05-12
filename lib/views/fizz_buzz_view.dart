37680dc170597e566f8723e47d350069cac42ffd
Carlos A. Falcon <karloalejandro9703@gmail.com>
2022-05-12 17:27:08 -0400

    add responsive solution to web and vertical view in mobile

--- lib/views/fizz_buzz_view.dart
+++ lib/views/fizz_buzz_view.dart
@@ -8,6 +8,7 @@ class FizzBuzzView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final width = MediaQuery.of(context).size.width;
     return GetBuilder(
       init: FizzBuzzController(),
       builder: (FizzBuzzController controller) => Scaffold(
@@ -23,52 +24,57 @@ class FizzBuzzView extends StatelessWidget {
               "Fizz Buzz Game",
             )),
         body: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 25.0, vertical: 10),
+          padding: EdgeInsets.symmetric(
+              horizontal: width > 550 ? width * 0.1 : 25.0, vertical: 10),
           child: Obx(
-            () => Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                // Introduccion Text
-                RichText(
-                    textAlign: TextAlign.center,
-                    text: TextSpan(
-                        text:
-                            "Introduce a number in the input to see if it's a Fizz, Buzz or FizzBuzz.",
-                        style: Get.textTheme.headline5)),
+            () => SizedBox(
+              width: double.infinity,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  // Introduccion Text
+                  RichText(
+                      textAlign: TextAlign.center,
+                      text: TextSpan(
+                          text:
+                              "Introduce a number in the input to see if it's a Fizz, Buzz or FizzBuzz.",
+                          style: Get.textTheme.headline5)),
 
-                //Input
-                Padding(
-                  padding: const EdgeInsets.symmetric(
-                      vertical: 25.0, horizontal: 15),
-                  child: Form(
-                      key: controller.formKey,
-                      child: InputNumberWidget(
-                        isValid: controller.isValid,
-                        onChange: controller.onChangedTextFormField,
-                      )),
-                ),
-
-                //Result
-                FittedBox(
-                  fit: BoxFit.scaleDown,
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(
-                        "Result:",
-                        style: Get.textTheme.headline4,
-                      ),
-                      const SizedBox(
-                        width: 10,
-                      ),
-                      Text(
-                        controller.response.value,
-                        style: Get.textTheme.headline4,
-                      ),
-                    ],
+                  //Input
+                  Container(
+                    width: 350,
+                    padding: const EdgeInsets.symmetric(
+                        vertical: 25.0, horizontal: 50.0),
+                    child: Form(
+                        key: controller.formKey,
+                        child: InputNumberWidget(
+                          isValid: controller.isValid,
+                          onChange: controller.onChangedTextFormField,
+                        )),
                   ),
-                )
-              ],
+
+                  //Result
+                  FittedBox(
+                    fit: BoxFit.scaleDown,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          "Result:",
+                          style: Get.textTheme.headline4,
+                        ),
+                        const SizedBox(
+                          width: 10,
+                        ),
+                        Text(
+                          controller.response.value,
+                          style: Get.textTheme.headline4,
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              ),
             ),
           ),
         ),
