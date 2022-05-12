79c769eeb852f5acb406ca0ff5ddf227533111a3
Carlos A. Falcon <karloalejandro9703@gmail.com>
2022-05-12 14:30:09 -0400

    minor fixes and add splash screen

--- lib/views/fizz_buzz_view.dart
+++ lib/views/fizz_buzz_view.dart
@@ -10,67 +10,65 @@ class FizzBuzzView extends StatelessWidget {
   Widget build(BuildContext context) {
     return GetBuilder(
       init: FizzBuzzController(),
-      builder: (FizzBuzzController controller) => SafeArea(
-        child: Scaffold(
-          appBar: AppBar(
-              automaticallyImplyLeading: false,
-              centerTitle: true,
-              elevation: 8,
-              shape: const RoundedRectangleBorder(
-                  borderRadius: BorderRadius.only(
-                      bottomLeft: Radius.circular(15),
-                      bottomRight: Radius.circular(15))),
-              title: const Text(
-                "Fizz Buzz Game",
-              )),
-          body: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 25.0, vertical: 10),
-            child: Obx(
-              () => Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  // Introduccion Text
-                  RichText(
-                      textAlign: TextAlign.center,
-                      text: TextSpan(
-                          text:
-                              "Introduce a number in the input to see if it's a Fizz, Buzz or FizzBuzz.",
-                          style: Get.textTheme.headline5)),
+      builder: (FizzBuzzController controller) => Scaffold(
+        appBar: AppBar(
+            automaticallyImplyLeading: false,
+            centerTitle: true,
+            elevation: 8,
+            shape: const RoundedRectangleBorder(
+                borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(15),
+                    bottomRight: Radius.circular(15))),
+            title: const Text(
+              "Fizz Buzz Game",
+            )),
+        body: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 25.0, vertical: 10),
+          child: Obx(
+            () => Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                // Introduccion Text
+                RichText(
+                    textAlign: TextAlign.center,
+                    text: TextSpan(
+                        text:
+                            "Introduce a number in the input to see if it's a Fizz, Buzz or FizzBuzz.",
+                        style: Get.textTheme.headline5)),
 
-                  //Input
-                  Padding(
-                    padding: const EdgeInsets.symmetric(
-                        vertical: 25.0, horizontal: 15),
-                    child: Form(
-                        key: controller.formKey,
-                        child: InputNumberWidget(
-                          isValid: controller.isValid,
-                          onChange: controller.onChangedTextFormField,
-                        )),
+                //Input
+                Padding(
+                  padding: const EdgeInsets.symmetric(
+                      vertical: 25.0, horizontal: 15),
+                  child: Form(
+                      key: controller.formKey,
+                      child: InputNumberWidget(
+                        isValid: controller.isValid,
+                        onChange: controller.onChangedTextFormField,
+                      )),
+                ),
+
+                //Result
+                FittedBox(
+                  fit: BoxFit.scaleDown,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        "Result:",
+                        style: Get.textTheme.headline4,
+                      ),
+                      const SizedBox(
+                        width: 10,
+                      ),
+                      Text(
+                        controller.response.value,
+                        style: Get.textTheme.headline4,
+                      ),
+                    ],
                   ),
-
-                  //Result
-                  FittedBox(
-                    fit: BoxFit.scaleDown,
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text(
-                          "Result:",
-                          style: Get.textTheme.headline4,
-                        ),
-                        const SizedBox(
-                          width: 10,
-                        ),
-                        Text(
-                          controller.response.value,
-                          style: Get.textTheme.headline4,
-                        ),
-                      ],
-                    ),
-                  )
-                ],
-              ),
+                )
+              ],
             ),
           ),
         ),
